3859c7c625d1d6adaa25b92eaa1601322bfd539d
Chris Pyles <cpyles@berkeley.edu>
2020-04-21 21:46:26 -0700

    put in try/except

--- gofer/ok.py
+++ gofer/ok.py
@@ -340,44 +340,49 @@ def check(test_file_path, global_env=None):
         global_env = inspect.currentframe().f_back.f_globals
     test_result = tests.run(global_env, include_grade=False)
 
-    pynb_path = glob.glob("*.ipynb")[0]  # assumes pynb always in dir
+    # put in try/except so that failure doesn't prevent students from moving on
+    try:
+        pynb_path = glob.glob("*.ipynb")[0]  # assumes pynb always in dir
+        
+        # find request for userID -> tornado call in gofer service
+        # gets the relevant metadata if its a pynb?
+
+        with open(pynb_path) as f:
+            nb = json.load(f)
+
+        exclude_last = global_env["_ih"][:-1] # exclude current cell
+        traceback = [s.split("\n") for s in exclude_last]
+        traceback.reverse()
+        vars = []
+        for cell in traceback:
+            for line in cell:
+                match = re.match(r"^(\w+\.?\w*)\s*=\s*.*$", line)
+                if match:
+                    if "." in match[1]:
+                        val = match[1].split(".")[0]
+                    else:
+                        val = match[1]
+                    if val not in vars:
+                        vars.append(val)
+            if any([re.match(r"check\(.*\)", l) for l in cell]):
+                break
+                    
+        vars = {var : global_env[var] if var in global_env else None for var in vars}
+
+        if 'assignment' not in nb['metadata']:
+            nb['metadata']['assignment'] = nb['metadata']['lab']
+
+        asyncio.ensure_future(log_check(
+            test_result.tests[0].name,
+            global_env["_i"],
+            vars,
+            nb["metadata"]["assignment"],
+            nb["metadata"]["section"],
+            3
+        ))
     
-    # find request for userID -> tornado call in gofer service
-    # gets the relevant metadata if its a pynb?
-
-    with open(pynb_path) as f:
-        nb = json.load(f)
-
-    exclude_last = global_env["_ih"][:-1] # exclude current cell
-    traceback = [s.split("\n") for s in exclude_last]
-    traceback.reverse()
-    vars = []
-    for cell in traceback:
-        for line in cell:
-            match = re.match(r"^(\w+\.?\w*)\s*=\s*.*$", line)
-            if match:
-                if "." in match[1]:
-                    val = match[1].split(".")[0]
-                else:
-                    val = match[1]
-                if val not in vars:
-                    vars.append(val)
-        if any([re.match(r"check\(.*\)", l) for l in cell]):
-            break
-                
-    vars = {var : global_env[var] if var in global_env else None for var in vars}
-
-    # TODO: pare down global env
-    if 'assignment' not in nb['metadata']:
-        nb['metadata']['assignment'] = nb['metadata']['lab']
-
-    asyncio.ensure_future(log_check(
-        test_result.tests[0].name,
-        global_env["_i"],
-        vars,
-        nb["metadata"]["assignment"],
-        nb["metadata"]["section"],
-        3
-    ))
+    except:
+        # ignore failure
+        pass
     
     return test_result
